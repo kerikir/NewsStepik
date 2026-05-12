803d56b318ac15d01388f04cdcb1b7bbc13e33bf
kerikir <vlasov-kerik@mail.ru>
2026-05-12 15:13:42 +0300

    repository interface added to the domain layer

---
+++ app/src/main/java/com/kerikir/news/domain/repository/NewsRepository.kt
@@ -0,0 +1,21 @@
+package com.kerikir.news.domain.repository
+
+import com.kerikir.news.domain.entity.Article
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+
+    fun getAllSubscriptions(): Flow<List<String>>
+
+    suspend fun addSubscription(topic: String)
+
+    suspend fun updateArticlesForTopic(topic: String)
+
+    suspend fun removeSubscription(topic: String)
+
+    suspend fun updateArticlesForAllSubscription()
+
+    fun getArticlesByTopics(topics: List<String>): Flow<List<Article>>
+
+    suspend fun clearAllArticles(topics: List<String>)
+}
