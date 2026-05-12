af2b6652085fe54a8f109f239150fb880b7a7221
kerikir <vlasov-kerik@mail.ru>
2026-05-12 15:51:29 +0300

    added a Use Case to retrieve all articles on the desired topics

---
+++ app/src/main/java/com/kerikir/news/domain/usecase/GetArticlesByTopicsUseCase.kt
@@ -0,0 +1,15 @@
+package com.kerikir.news.domain.usecase
+
+import com.kerikir.news.domain.entity.Article
+import com.kerikir.news.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetArticlesByTopicsUseCase @Inject constructor(
+    private val newsRepository: NewsRepository
+) {
+
+    operator fun invoke(topics: List<String>): Flow<List<Article>> {
+        return newsRepository.getArticlesByTopics(topics)
+    }
+}
