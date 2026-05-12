2bc58568a5dcda61137f4d1b5d3ff975641cccbc
kerikir <vlasov-kerik@mail.ru>
2026-05-12 15:46:16 +0300

    added a Use Case for updating articles for all topics

---
+++ app/src/main/java/com/kerikir/news/domain/usecase/UpdateSubscribedArticlesUseCase.kt
@@ -0,0 +1,13 @@
+package com.kerikir.news.domain.usecase
+
+import com.kerikir.news.domain.repository.NewsRepository
+import javax.inject.Inject
+
+class UpdateSubscribedArticlesUseCase @Inject constructor(
+    private val newsRepository: NewsRepository
+) {
+
+    suspend operator fun invoke() {
+        newsRepository.updateArticlesForAllSubscription()
+    }
+}
