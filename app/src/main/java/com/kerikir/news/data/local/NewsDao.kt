4a9deb6ae28066ec57a8bc7df806c2e01cd8e65f
kerikir <vlasov-kerik@mail.ru>
2026-05-13 13:52:21 +0300

    methods have been added to work with the articles table in the database

--- app/src/main/java/com/kerikir/news/data/local/NewsDao.kt
+++ app/src/main/java/com/kerikir/news/data/local/NewsDao.kt
@@ -19,5 +19,12 @@ interface NewsDao {
     @Delete
     suspend fun deleteSubscription(subscriptionDbModel: SubscriptionDbModel)
 
-    
+    @Query("SELECT * FROM articles WHERE topic IN (:topics) ORDER BY publishedAt DESC")
+    fun getAllArticlesByTopics(topics: List<String>): Flow<List<ArticleDbModel>>
+
+    @Insert(onConflict = IGNORE)
+    suspend fun addArticles(articles: List<ArticleDbModel>)
+
+    @Query("DELETE FROM articles WHERE topic IN (:topics)")
+    suspend fun deleteArticlesByTopics(topics: List<String>)
 }
