dc036b56bd0359c045f47468bd5cdec16dbdbb16
kerikir <vlasov-kerik@mail.ru>
2026-05-13 14:13:08 +0300

    added the DI module for the data layer

---
+++ app/src/main/java/com/kerikir/news/di/DataModule.kt
@@ -0,0 +1,31 @@
+package com.kerikir.news.di
+
+import android.content.Context
+import androidx.room.Room
+import com.kerikir.news.data.local.NewsDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface DataModule {
+
+    companion object {
+
+        @Provides
+        @Singleton
+        fun provideNewsDatabase(
+            @ApplicationContext context: Context
+        ): NewsDatabase {
+            return Room.databaseBuilder(
+                context = context,
+                klass = NewsDatabase::class.java,
+                name = "news.db"
+            ).fallbackToDestructiveMigration(true).build()
+        }
+    }
+}
