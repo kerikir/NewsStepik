12aff166903f901d567bff51fed98c95acb11364
kerikir <vlasov-kerik@mail.ru>
2026-05-19 00:33:21 +0300

    delete the unnecessary code

--- app/src/main/java/com/kerikir/news/data/remote/Source.kt
+++ app/src/main/java/com/kerikir/news/data/remote/Source.kt
@@ -6,8 +6,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Source(
-    @SerialName("id")
-    val id: String? = "",
     @SerialName("name")
     val name: String = ""
 )
