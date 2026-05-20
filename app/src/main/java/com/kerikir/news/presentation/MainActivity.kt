99b675c2f3414614e52a2976701ab4ee65f59d28
kerikir <vlasov-kerik@mail.ru>
2026-05-20 20:22:23 +0300

    deleted the unnecessary code

--- app/src/main/java/com/kerikir/news/presentation/MainActivity.kt
+++ app/src/main/java/com/kerikir/news/presentation/MainActivity.kt
@@ -1,30 +1,18 @@
 package com.kerikir.news.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.lifecycle.lifecycleScope
-import com.kerikir.news.data.remote.NewsApiService
 import com.kerikir.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject
-    lateinit var newsApiService: NewsApiService
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        lifecycleScope.launch {
-            val response = newsApiService.loadArticles("Kotlin")
-            Log.d("MainActivity", response.toString())
-        }
         setContent {
             NewsTheme {
             }
