ccb9310903159c3440bf0b99996b3d2dfd518221
kerikir <vlasov-kerik@mail.ru>
2026-05-20 17:49:22 +0300

    added getting an instance of API Service

--- app/src/main/java/com/kerikir/news/di/DataModule.kt
+++ app/src/main/java/com/kerikir/news/di/DataModule.kt
@@ -4,11 +4,17 @@ import android.content.Context
 import androidx.room.Room
 import com.kerikir.news.data.local.NewsDao
 import com.kerikir.news.data.local.NewsDatabase
+import com.kerikir.news.data.remote.NewsApiService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -16,6 +22,21 @@ import javax.inject.Singleton
 interface DataModule {
 
     companion object {
+
+        @Provides
+        @Singleton
+        fun provideApiService(): NewsApiService {
+            val baseUrl = "https://newsapi.org/"
+            val converter = Json.asConverterFactory(
+                "application/json".toMediaType()
+            )
+            val retrofit = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(converter)
+                .build()
+            return retrofit.create()
+        }
+
 
         @Provides
         @Singleton
