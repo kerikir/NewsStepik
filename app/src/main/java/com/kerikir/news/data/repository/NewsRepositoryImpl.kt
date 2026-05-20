cfb2dac5dbd9718e4d32bf827abc537d8aa8f6fa
kerikir <vlasov-kerik@mail.ru>
2026-05-20 23:06:24 +0300

    added implementation of methods in the repository

--- app/src/main/java/com/kerikir/news/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/kerikir/news/data/repository/NewsRepositoryImpl.kt
@@ -1,19 +1,27 @@
 package com.kerikir.news.data.repository
 
+import com.kerikir.news.data.local.NewsDao
+import com.kerikir.news.data.local.SubscriptionDbModel
+import com.kerikir.news.data.remote.NewsApiService
 import com.kerikir.news.domain.entity.Article
 import com.kerikir.news.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class NewsRepositoryImpl @Inject constructor(
-
+    private val newsDao: NewsDao,
+    private val newsApiService: NewsApiService
 ) : NewsRepository {
+
     override fun getAllSubscriptions(): Flow<List<String>> {
-        TODO("Not yet implemented")
+        return newsDao.getAllSubscriptions().map {  subscriptions ->
+            subscriptions.map { it.topic }
+        }
     }
 
     override suspend fun addSubscription(topic: String) {
-        TODO("Not yet implemented")
+        newsDao.addSubscription(SubscriptionDbModel(topic))
     }
 
     override suspend fun updateArticlesForTopic(topic: String) {
