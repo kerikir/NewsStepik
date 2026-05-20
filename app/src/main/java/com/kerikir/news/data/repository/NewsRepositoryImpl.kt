cca7b2bc5fb4d578c07ce54f18bb5c5c97abd04b
kerikir <vlasov-kerik@mail.ru>
2026-05-20 21:44:35 +0300

    added repository implementation

---
+++ app/src/main/java/com/kerikir/news/data/repository/NewsRepositoryImpl.kt
@@ -0,0 +1,35 @@
+package com.kerikir.news.data.repository
+
+import com.kerikir.news.domain.entity.Article
+import com.kerikir.news.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
+
+class NewsRepositoryImpl : NewsRepository {
+    override fun getAllSubscriptions(): Flow<List<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addSubscription(topic: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateArticlesForTopic(topic: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun removeSubscription(topic: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateArticlesForAllSubscription() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getArticlesByTopics(topics: List<String>): Flow<List<Article>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearAllArticles(topics: List<String>) {
+        TODO("Not yet implemented")
+    }
+}
