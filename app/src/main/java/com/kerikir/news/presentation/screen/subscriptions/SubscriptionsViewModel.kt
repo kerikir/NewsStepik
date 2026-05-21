085bebf9e4f168d5ea1afabf789f004b542afc91
kerikir <vlasov-kerik@mail.ru>
2026-05-21 13:26:11 +0300

    added the Subscriptions Command interface

---
+++ app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsViewModel.kt
@@ -0,0 +1,22 @@
+package com.kerikir.news.presentation.screen.subscriptions
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SubscriptionsViewModel: ViewModel() {
+
+
+}
+
+
+sealed interface SubscriptionsCommand {
+
+    data class InputTopic(val query: String) : SubscriptionsCommand
+
+    data object ClickSubscribe : SubscriptionsCommand
+
+    data object RefreshData : SubscriptionsCommand
+
+    data class ToggleTopicSelection(val topic: String) : SubscriptionsCommand
+}
