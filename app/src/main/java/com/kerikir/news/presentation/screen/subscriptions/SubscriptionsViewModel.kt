b6004bd09b4b984f1487dd369fbebcc6491bb379
kerikir <vlasov-kerik@mail.ru>
2026-05-21 13:53:56 +0300

    added a class to describe the screen state

--- app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsViewModel.kt
+++ app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsViewModel.kt
@@ -1,6 +1,7 @@
 package com.kerikir.news.presentation.screen.subscriptions
 
 import androidx.lifecycle.ViewModel
+import com.kerikir.news.domain.entity.Article
 import dagger.hilt.android.lifecycle.HiltViewModel
 
 @HiltViewModel
@@ -8,6 +9,7 @@ class SubscriptionsViewModel: ViewModel() {
 
 
 }
+
 
 
 sealed interface SubscriptionsCommand {
@@ -23,4 +25,19 @@ sealed interface SubscriptionsCommand {
     data object ClearArticles : SubscriptionsCommand
 
     data class RemoveSubscription(val topic: String) : SubscriptionsCommand
+}
+
+
+
+data class SubscriptionsState(
+    val query: String = "",
+    val subscriptions: Map<String, Boolean> = mapOf(),
+    val articles: List<Article> = listOf()
+) {
+
+    val subscribeButtonEnabled: Boolean
+        get() = query.isNotBlank()
+
+    val selectedTopics: List<String>
+        get() = subscriptions.filter { it.value }.map { it.key }
 }
