61f27a3a8bd0c70b6a0838062ecdf1d1e2b146d2
kerikir <vlasov-kerik@mail.ru>
2026-05-21 14:08:28 +0300

    added the Subscriptions View Model class

--- app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsViewModel.kt
+++ app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsViewModel.kt
@@ -2,12 +2,29 @@ package com.kerikir.news.presentation.screen.subscriptions
 
 import androidx.lifecycle.ViewModel
 import com.kerikir.news.domain.entity.Article
+import com.kerikir.news.domain.usecase.AddSubscriptionUseCase
+import com.kerikir.news.domain.usecase.ClearAllArticlesUseCase
+import com.kerikir.news.domain.usecase.GetAllSubscriptionsUseCase
+import com.kerikir.news.domain.usecase.GetArticlesByTopicsUseCase
+import com.kerikir.news.domain.usecase.RemoveSubscriptionUseCase
+import com.kerikir.news.domain.usecase.UpdateSubscribedArticlesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class SubscriptionsViewModel: ViewModel() {
+class SubscriptionsViewModel @Inject constructor(
+    private val addSubscriptionUseCase: AddSubscriptionUseCase,
+    private val clearAllArticlesUseCase: ClearAllArticlesUseCase,
+    private val getAllSubscriptionsUseCase: GetAllSubscriptionsUseCase,
+    private val getArticlesByTopicsUseCase: GetArticlesByTopicsUseCase,
+    private val removeSubscriptionUseCase: RemoveSubscriptionUseCase,
+    private val updateSubscribedArticlesUseCase: UpdateSubscribedArticlesUseCase
+): ViewModel() {
 
-
+    private val _state = MutableStateFlow(SubscriptionsState())
+    val state = _state.asStateFlow()
 }
 
 
