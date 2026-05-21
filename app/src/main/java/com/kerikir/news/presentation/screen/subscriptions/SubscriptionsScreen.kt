5eb20860004d78d0d22852aafe5aba8040cb4f4e
kerikir <vlasov-kerik@mail.ru>
2026-05-21 17:34:29 +0300

    added the composable function Subscriptions Screen

---
+++ app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsScreen.kt
@@ -0,0 +1,12 @@
+package com.kerikir.news.presentation.screen.subscriptions
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun SubscriptionsScreen(
+    modifier: Modifier = Modifier,
+    onNavigateToSettings: () -> Unit,
+    viewModel: SubscriptionsViewModel = hiltViewModel()
+) {}
