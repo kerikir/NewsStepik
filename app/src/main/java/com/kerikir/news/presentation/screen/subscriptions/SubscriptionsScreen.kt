cb7904168ed7097d6d3d652fcf04d52246649810
kerikir <vlasov-kerik@mail.ru>
2026-05-22 19:38:31 +0300

    added articles to the subscriptions screen

--- app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsScreen.kt
+++ app/src/main/java/com/kerikir/news/presentation/screen/subscriptions/SubscriptionsScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -115,6 +116,12 @@ fun SubscriptionsScreen(
                 }
                 item {
                     HorizontalDivider()
+                }
+                items(
+                    items = state.articles,
+                    key = { it.url }
+                ) {
+                    ArticleCard(article = it)
                 }
             }
         }
