5e1e6c73ddb806c6219ad7e3da6fc5ce4d301051
kerikir <vlasov-kerik@mail.ru>
2026-05-26 19:57:32 +0300

    added a repository interface for settings

---
+++ app/src/main/java/com/kerikir/news/domain/repository/SettingsRepository.kt
@@ -0,0 +1,18 @@
+package com.kerikir.news.domain.repository
+
+import com.kerikir.news.domain.entity.Language
+import com.kerikir.news.domain.entity.Settings
+import kotlinx.coroutines.flow.Flow
+
+interface SettingsRepository {
+
+    fun getSettings(): Flow<Settings>
+
+    suspend fun updateLanguage(language: Language)
+
+    suspend fun updateInterval(minutes: Int)
+
+    suspend fun updateNotificationsEnables(enabled: Boolean)
+
+    suspend fun updateWifiOnly(wifiOnly: Boolean)
+}
