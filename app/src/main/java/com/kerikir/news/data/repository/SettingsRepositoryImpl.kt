694c22df472bed1771716357f3b76b9ccbc20034
kerikir <vlasov-kerik@mail.ru>
2026-05-27 17:20:54 +0300

    added the use of DataStore in the repository

--- app/src/main/java/com/kerikir/news/data/repository/SettingsRepositoryImpl.kt
+++ app/src/main/java/com/kerikir/news/data/repository/SettingsRepositoryImpl.kt
@@ -1,14 +1,31 @@
 package com.kerikir.news.data.repository
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.kerikir.news.domain.entity.Language
 import com.kerikir.news.domain.entity.Settings
 import com.kerikir.news.domain.repository.SettingsRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
+
+
 class SettingsRepositoryImpl @Inject constructor(
 
 ) : SettingsRepository {
+
+    private val languageKey = stringPreferencesKey("language")
+    private val intervalKey = intPreferencesKey("interval")
+    private val notificationsEnabledKey = booleanPreferencesKey("notifications_enabled")
+    private val wifiOnlyKey = booleanPreferencesKey("wifi_only")
+
 
     override fun getSettings(): Flow<Settings> {
         TODO("Not yet implemented")
