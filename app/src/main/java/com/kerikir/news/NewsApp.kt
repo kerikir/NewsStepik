7684ad13d14ccf7e01c96072786eb9927ab369f1
kerikir <vlasov-kerik@mail.ru>
2026-05-28 19:24:27 +0300

    added an instance of the class for running background updates to the application class

--- app/src/main/java/com/kerikir/news/NewsApp.kt
+++ app/src/main/java/com/kerikir/news/NewsApp.kt
@@ -3,6 +3,7 @@ package com.kerikir.news
 import android.app.Application
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import com.kerikir.news.presentation.startup.AppStartupManager
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -11,6 +12,9 @@ class NewsApp : Application(), Configuration.Provider {
 
     @Inject
     lateinit var workerFactory: HiltWorkerFactory
+
+    @Inject
+    lateinit var appStartupManager: AppStartupManager
 
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
