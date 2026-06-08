fd3e380a156809ceeb24b77af088d3a693280fe0
kerikir <vlasov-kerik@mail.ru>
2026-06-08 21:14:19 +0300

    added a method for displaying notifications

--- app/src/main/java/com/kerikir/news/data/background/NotificationsHelper.kt
+++ app/src/main/java/com/kerikir/news/data/background/NotificationsHelper.kt
@@ -4,6 +4,7 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
 import com.kerikir.news.R
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -33,8 +34,26 @@ class NotificationsHelper @Inject constructor(
     }
 
 
+    fun showNewArticlesNotification(topics: List<String>) {
+        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_breaking_news)
+            .setContentTitle(context.getString(R.string.new_articles_notification_title))
+            .setContentText(
+                context.getString(
+                    R.string.update_subscriptions,
+                    topics.size,
+                    topics.joinToString(", ")
+                )
+            )
+            .build()
+
+        notificationManager?.notify(NOTIFICATION_ID, notification)
+    }
+
+
     companion object {
 
         private const val CHANNEL_ID = "new_articles"
+        private const val NOTIFICATION_ID = 1
     }
 }
