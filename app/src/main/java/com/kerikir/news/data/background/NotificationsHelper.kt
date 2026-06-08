6dc5cfc7201175207709c60167ce6801e6231c8f
kerikir <vlasov-kerik@mail.ru>
2026-06-08 20:05:50 +0300

    added the creation of a notification channel

--- app/src/main/java/com/kerikir/news/data/background/NotificationsHelper.kt
+++ app/src/main/java/com/kerikir/news/data/background/NotificationsHelper.kt
@@ -1,14 +1,40 @@
 package com.kerikir.news.data.background
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import androidx.core.content.getSystemService
+import com.kerikir.news.R
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class NotificationsHelper @Inject constructor(
-    @ApplicationContext context: Context
+    @param:ApplicationContext private val context: Context
 ) {
 
     private val notificationManager = context.getSystemService<NotificationManager>()
+
+
+    init {
+        createNotificationChannel()
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                context.getString(R.string.new_articles),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
+
+    companion object {
+
+        private const val CHANNEL_ID = "new_articles"
+    }
 }
