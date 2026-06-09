14c8e0b821f2007104eb744745c062579431929e
kerikir <vlasov-kerik@mail.ru>
2026-06-09 19:52:33 +0300

    pending intent added to notification

--- app/src/main/java/com/kerikir/news/data/background/NotificationsHelper.kt
+++ app/src/main/java/com/kerikir/news/data/background/NotificationsHelper.kt
@@ -2,11 +2,14 @@ package com.kerikir.news.data.background
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.content.getSystemService
 import com.kerikir.news.R
+import com.kerikir.news.presentation.MainActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -35,9 +38,20 @@ class NotificationsHelper @Inject constructor(
 
 
     fun showNewArticlesNotification(topics: List<String>) {
+        val intent = Intent(context, MainActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            PENDING_INTENT_RC,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_breaking_news)
             .setContentTitle(context.getString(R.string.new_articles_notification_title))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .setContentText(
                 context.getString(
                     R.string.update_subscriptions,
@@ -55,5 +69,7 @@ class NotificationsHelper @Inject constructor(
 
         private const val CHANNEL_ID = "new_articles"
         private const val NOTIFICATION_ID = 1
+
+        private const val PENDING_INTENT_RC = 1
     }
 }
