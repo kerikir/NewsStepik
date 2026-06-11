302d4b469db137316b35add64c635017fdbda6f1
kerikir <vlasov-kerik@mail.ru>
2026-06-11 18:01:02 +0300

    updated the Use Case for determining the language of articles

--- app/src/main/java/com/kerikir/news/domain/usecase/AddSubscriptionUseCase.kt
+++ app/src/main/java/com/kerikir/news/domain/usecase/AddSubscriptionUseCase.kt
@@ -1,19 +1,23 @@
 package com.kerikir.news.domain.usecase
 
 import com.kerikir.news.domain.repository.NewsRepository
+import com.kerikir.news.domain.repository.SettingsRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class AddSubscriptionUseCase @Inject constructor(
-    private val newsRepository: NewsRepository
+    private val newsRepository: NewsRepository,
+    private val settingsRepository: SettingsRepository
 ) {
 
     suspend operator fun invoke(topic: String) {
         newsRepository.addSubscription(topic)
         CoroutineScope(currentCoroutineContext()).launch {
-            newsRepository.updateArticlesForTopic(topic)
+            val settings = settingsRepository.getSettings().first()
+            newsRepository.updateArticlesForTopic(topic, settings.language)
         }
     }
 }
