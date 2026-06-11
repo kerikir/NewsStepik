4c9027c0a8cf4f735b2882e145f43db07f43f316
kerikir <vlasov-kerik@mail.ru>
2026-06-11 17:06:51 +0300

    added a check in Worker whether notifications are enabled

--- app/src/main/java/com/kerikir/news/data/background/RefreshDataWorker.kt
+++ app/src/main/java/com/kerikir/news/data/background/RefreshDataWorker.kt
@@ -5,22 +5,26 @@ import android.util.Log
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.kerikir.news.domain.usecase.GetSettingsUseCase
 import com.kerikir.news.domain.usecase.UpdateSubscribedArticlesUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.first
 
 @HiltWorker
 class RefreshDataWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted workerParameters: WorkerParameters,
-    private val updateSubscribedArticlesUseCase: UpdateSubscribedArticlesUseCase
-    private val notificationsHelper: NotificationsHelper
+    private val updateSubscribedArticlesUseCase: UpdateSubscribedArticlesUseCase,
+    private val notificationsHelper: NotificationsHelper,
+    private val getSettingsUseCase: GetSettingsUseCase
 ) : CoroutineWorker(context, workerParameters) {
 
     override suspend fun doWork(): Result {
         Log.d("RefreshDataWorker", "Start")
+        val settings = getSettingsUseCase().first()
         val updatedTopics =  updateSubscribedArticlesUseCase()
-        if (updatedTopics.isNotEmpty()) {
+        if (updatedTopics.isNotEmpty() && settings.notificationsEnabled) {
             notificationsHelper.showNewArticlesNotification(updatedTopics)
         }
         Log.d("RefreshDataWorker", "Finish")
