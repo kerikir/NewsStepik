6d87aca180368cebff616d6e1bf65d89b653c838
kerikir <vlasov-kerik@mail.ru>
2026-06-12 20:50:13 +0300

    added a sealed interface for the settings screen state

--- app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsViewModel.kt
+++ app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsViewModel.kt
@@ -1,6 +1,8 @@
 package com.kerikir.news.presentation.screen.settings
 
 import androidx.lifecycle.ViewModel
+import com.kerikir.news.domain.entity.Interval
+import com.kerikir.news.domain.entity.Language
 import com.kerikir.news.domain.usecase.GetSettingsUseCase
 import com.kerikir.news.domain.usecase.UpdateIntervalUseCase
 import com.kerikir.news.domain.usecase.UpdateLanguageUseCase
@@ -16,4 +18,20 @@ class SettingsViewModel @Inject constructor(
     private val updateNotificationsEnabledUseCase: UpdateNotificationsEnabledUseCase,
     private val updateWifiOnlyUseCase: UpdateWifiOnlyUseCase
 ) : ViewModel() {
+}
+
+
+
+sealed interface SettingsState {
+
+    data object Initial : SettingsState
+
+    data class Configuration(
+        val language: Language,
+        val interval: Interval,
+        val wifiOnly: Boolean,
+        val notificationsEnable: Boolean,
+        val languages: List<Language> = Language.entries,
+        val intervals: List<Interval> = Interval.entries
+    ) : SettingsState
 }
