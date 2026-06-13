7c4ae6b07ff51d333e08bede4e7caebd7cee07a0
kerikir <vlasov-kerik@mail.ru>
2026-06-13 22:15:40 +0300

    added a title and a subtitle to the settings card

--- app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsScreen.kt
@@ -1,7 +1,17 @@
 package com.kerikir.news.presentation.screen.settings
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 
 
@@ -20,4 +30,23 @@ private fun SettingsCard(
     title: String,
     subtitle: String,
     content: @Composable () -> Unit
-) {}
+) {
+    Card(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = subtitle,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+    }
+}
