cd762cf3d71c63e0a54f30cfdb2dbce5cda8822f
kerikir <vlasov-kerik@mail.ru>
2026-06-14 17:37:01 +0300

    added the display of the selected drop-down list option

--- app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.kerikir.news.presentation.screen.settings
 
 import androidx.compose.foundation.layout.Column
@@ -6,9 +8,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -62,4 +73,24 @@ private fun<T> SettingsDropdown(
     selectedItem: T,
     onItemSelected: (T) -> Unit,
     itemAsString: (T) -> String
-) {}
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        modifier = modifier.fillMaxWidth(),
+        expanded = expanded,
+        onExpandedChange = { expanded = it }
+    ) {
+
+        TextField(
+            modifier = Modifier.fillMaxWidth()
+                .menuAnchor(MenuAnchorType.PrimaryNotEditable, true),
+            value = itemAsString(selectedItem),
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            }
+        )
+    }
+}
