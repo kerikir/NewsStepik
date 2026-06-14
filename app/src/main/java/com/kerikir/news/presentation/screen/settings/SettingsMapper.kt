2cd2cc6627dedfca8bf8e28604acba25caf1c0d9
kerikir <vlasov-kerik@mail.ru>
2026-06-14 18:49:09 +0300

    added language names to string resources

--- app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsMapper.kt
+++ app/src/main/java/com/kerikir/news/presentation/screen/settings/SettingsMapper.kt
@@ -1,15 +1,17 @@
 package com.kerikir.news.presentation.screen.settings
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.kerikir.news.R
 import com.kerikir.news.domain.entity.Language
 
 
 @Composable
 fun Language.toReadableFormat(): String {
     return when(this) {
-        Language.ENGLISH -> "English"
-        Language.RUSSIAN -> "Русский"
-        Language.FRENCH -> "Français"
-        Language.GERMAN -> "Deutsch"
+        Language.ENGLISH -> stringResource(R.string.english)
+        Language.RUSSIAN -> stringResource(R.string.russian)
+        Language.FRENCH -> stringResource(R.string.french)
+        Language.GERMAN -> stringResource(R.string.german)
     }
 }
