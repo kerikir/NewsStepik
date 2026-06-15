789cc0f3ac589a773c581530d4424731d80bcc65
kerikir <vlasov-kerik@mail.ru>
2026-06-15 18:25:09 +0300

    deleted the unnecessary code

--- app/src/main/java/com/kerikir/news/presentation/MainActivity.kt
+++ app/src/main/java/com/kerikir/news/presentation/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.kerikir.news.presentation
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import com.kerikir.news.domain.repository.NewsRepository
-import com.kerikir.news.presentation.screen.subscriptions.SubscriptionsScreen
+import com.kerikir.news.presentation.navigation.NavGraph
 import com.kerikir.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -24,17 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsTheme {
-                val permissionLauncher = rememberLauncherForActivityResult(
-                    contract = ActivityResultContracts.RequestPermission(),
-                    onResult = {}
-                )
-                SubscriptionsScreen(
-                    onNavigateToSettings = {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                            permissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
-                        }
-                    }
-                )
+                NavGraph()
             }
         }
     }
