afd04d7b94c01a026b989521f704ba81a90fe075
kerikir <vlasov-kerik@mail.ru>
2026-06-15 18:04:15 +0300

    added a Composable function for the navigation graph

---
+++ app/src/main/java/com/kerikir/news/presentation/navigation/NavGraph.kt
@@ -0,0 +1,17 @@
+package com.kerikir.news.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination =
+    ) {
+        
+    }
+}
